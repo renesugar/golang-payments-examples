427e4229167c0fe7a8133e9ca31a33aa6910ca8e
Tristan Wietsma <tristan.wietsma@gmail.com>
2015-03-12 21:18:56 -0500

    plans

--- stripe/list-plans/main.go
+++ stripe/list-plans/main.go
@@ -3,18 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/stripe/stripe-go"
-	"github.com/stripe/stripe-go/customer"
+	"github.com/stripe/stripe-go/plan"
 	"os"
 )
 
 func main() {
 	stripe.Key = os.Getenv("STRIPE_PRIVATE_KEY")
 
-	// list all customers
-	i := customer.List(nil)
+	i := plan.List(nil)
 	for i.Next() {
-		c := i.Customer()
-		fmt.Println(c)
+		fmt.Println(i.Plan())
 	}
-
 }
